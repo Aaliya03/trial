3584ad31162358af6b80a0b9efe40a6f03baa7c4
Aaliya03 <aaliyar2003@gmail.com>
2023-09-06 09:48:27 +0530

    msh

--- frontend/my-app/src/h1.js
+++ frontend/my-app/src/h1.js
@@ -8,9 +8,11 @@
 // terminal
 // git init 
 // git config --global hhtp.proxy http://192.168.10.252.808
-// git config --global.user.name "aaliya"
-// git remote and origin "link from repositiory"
-// git add. #adding all the files
+// git config --global user.email "aaliyar2003@gmail.com"
+// git config --global user.name "Aaliya03"
+// git remote add origin "link from repositiory"
+// git remote -v 
+// git add . #adding all the files
 // git commit -m "msg" --commit will save changes
 // git push origin master
 // git status
